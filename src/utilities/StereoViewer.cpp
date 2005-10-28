50e95d42d3872e66cc21c3d8c7a6942839e52b4e
camilleg <>
2005-10-28 02:05:11 +0000

    major refactoring

--- src/utilities/StereoViewer.cpp
+++ src/utilities/StereoViewer.cpp
@@ -12,29 +12,29 @@
 string leftFile, rightFile;
 arTexture leftImage, rightImage;
 int screenWidth = -1, screenHeight = -1;
-int numImages = -1;
+bool twoImages = false;
 
 bool init(arMasterSlaveFramework&, arSZGClient& cli) {
   const string dataPath(cli.getAttribute( "SZG_DATA", "path" ));
   if (!leftImage.readPPM( leftFile, dataPath )){
-    cerr << "StereoViewer error: failed to read picture file \""
-         << leftFile << "\".\n";
+    cerr << "StereoViewer error: failed to read picture file \"" << leftFile
+           << "\" from path " << dataPath << ".\n";
     return false;
   }
   if (!leftImage.flipHorizontal()) {
-    cerr << "StereoViewer error: failed to flip picture " << leftFile << endl;
+    cerr << "StereoViewer error: failed to flip picture file " << leftFile << endl;
     return false;
   }
-  cerr << "Image size: " << leftImage.getWidth()/2 << " X "
+  cout << "StereoViewer remark: Image size " << leftImage.getWidth()/2 << " X "
        << leftImage.getHeight() << endl;
-  if (numImages == 2) {
+  if (twoImages) {
     if (!rightImage.readPPM( rightFile, dataPath )){
       cerr << "StereoViewer error: failed to read picture file " << rightFile
            << " from path " << dataPath << ".\n";
       return false;
     }
     if (!rightImage.flipHorizontal()) {
-      cerr << "StereoViewer error: failed to flip picture " << rightFile
+      cerr << "StereoViewer error: failed to flip picture file " << rightFile
            << endl;
       return false;
     }
@@ -44,89 +44,77 @@ bool init(arMasterSlaveFramework&, arSZGClient& cli) {
   return true;
 }
 
-void showCenteredImage( arTexture& theImage ) {
+void showImage( arTexture& theImage, bool fLeftHalf, bool fHalf ) {
   const int imageWidth = theImage.getWidth();
   const int imageHeight = theImage.getHeight();
-  int xOffset = 0;
-  int yOffset = 0;
-  if (imageWidth < screenWidth)
-    xOffset = (int)floor( 0.5*(screenWidth-imageWidth) );
-  if (imageHeight < screenHeight)
-    yOffset = (int)floor( 0.5*(screenHeight-imageHeight) );
+  const int xOffset = imageWidth >= screenWidth ? 0 :
+    (int)floor( 0.5*(screenWidth-imageWidth) );
+  const int yOffset = imageHeight >= screenHeight ? 0 :
+    (int)floor( 0.5*(screenHeight-imageHeight) );
   glRasterPos3i( xOffset, yOffset, 0 );
+
+  if (fHalf) {
+    glPixelStorei( GL_UNPACK_ROW_LENGTH, theImage.getWidth() );
+    glPixelStorei( GL_UNPACK_SKIP_PIXELS, fLeftHalf ? imageWidth : 0 );
+  }
+
   glDrawPixels( imageWidth, imageHeight, GL_RGB, GL_UNSIGNED_BYTE,
                 theImage.getPixels());
 }
 
-void showCenteredHalfImage( arTexture& theImage, bool leftHalf ) {
-  const int imageWidth = theImage.getWidth()/2;
-  const int imageHeight = theImage.getHeight();
-  int xOffset = 0;
-  int yOffset = 0;
-  if (imageWidth < screenWidth)
-    xOffset = (int)floor( 0.5*(screenWidth-imageWidth) );
-  if (imageHeight < screenHeight)
-    yOffset = (int)floor( 0.5*(screenHeight-imageHeight) );
-  glRasterPos3i( xOffset, yOffset, 0 );
-  glPixelStorei( GL_UNPACK_ROW_LENGTH, theImage.getWidth() );
-  glPixelStorei( GL_UNPACK_SKIP_PIXELS, leftHalf ? imageWidth : 0 );
-  glDrawPixels( imageWidth, imageHeight, GL_RGB, GL_UNSIGNED_BYTE,
-                theImage.getPixels());
-}
-
-void display( arMasterSlaveFramework& fw, arGraphicsWindow& graphicsWindow, arViewport& viewport ) {
+void display( arMasterSlaveFramework&, arGraphicsWindow& graphicsWindow, arViewport&) {
   glMatrixMode( GL_PROJECTION );
   glLoadIdentity();
   glOrtho( 0, screenWidth, 0, screenHeight, -1, 1 );
   glMatrixMode( GL_MODELVIEW );
   glLoadIdentity();
   glPixelStorei( GL_UNPACK_ALIGNMENT, 1 );
-
   const bool fLeftEye = graphicsWindow.getCurrentEyeSign() <= 0.0f; // fw.getCurrentEye() <= 0.;
-
-  if (numImages == 2)
-    showCenteredImage( fLeftEye ? leftImage : rightImage );
+  if (twoImages)
+    showImage( fLeftEye ? leftImage : rightImage, false, false); // full image
   else
-    showCenteredHalfImage( leftImage, fLeftEye );
+    showImage( leftImage, fLeftEye, true); // half image
 }
 
+#ifdef UNUSED
 void reshape(arMasterSlaveFramework&, int width, int height) {
   screenWidth = width;
   screenHeight = height;
   glViewport(0,0,width,height);
 }
+#endif
 
 void windowEvent( arMasterSlaveFramework& fw, arGUIWindowInfo* windowInfo ) {
-  if( !windowInfo ) {
+  if( !windowInfo )
     return;
-  }
 
   switch( windowInfo->getState() ) {
     case AR_WINDOW_RESIZE:
       screenWidth = windowInfo->getSizeY();
       screenHeight = windowInfo->getSizeX();
-      fw.getWindowManager()->setWindowViewport( windowInfo->getWindowID(),
-                                                0, 0, windowInfo->getSizeX(), windowInfo->getSizeY() );
+      fw.getWindowManager()->setWindowViewport(
+        windowInfo->getWindowID(), 0, 0, windowInfo->getSizeX(), windowInfo->getSizeY() );
     break;
-
-    default:
+  default: // avoid compiler warning
     break;
   }
 }
 
-
-
 int main(int argc, char** argv){
-  if (argc != 2 && argc != 3) {
+  twoImages = false;
+  switch (argc) {
+  case 3:
+    twoImages = true;
+    rightFile = argv[2];
+    // fallthrough
+  case 2:
+    leftFile = argv[1];
+    break;
+  default:
     cerr << "usage: StereoViewer left_image right_image\n"
          << "       StereoViewer stereo_pair_image\n";
     return 1;
   }
-
-  numImages = argc-1;
-  leftFile = argv[1];
-  if (numImages == 2)
-    rightFile = argv[2];
 
   arMasterSlaveFramework framework;
   if (!framework.init(argc, argv))
