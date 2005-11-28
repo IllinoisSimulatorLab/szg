a1a4b9564d17ed665b9f4d7d230df8866557ea42
schaeffr <>
2005-11-28 21:14:00 +0000

    working on scene graph python examples

--- doc/python/lorenz.py
+++ doc/python/lorenz.py
@@ -3,6 +3,45 @@ import sys
 import time
 import random
 
+class particle:
+	def __init__(self):
+		self.dt = 0.004
+		self.x = arVector3(8,8,23)
+		self.mat = arMaterial()
+		self.mat.diffuse = arVector3(1,0,0)
+		self.mat.specular = arVector3(0.2, 0.2, 0.2)
+		self.mat.emissive = arVector3(0,0,0.1)
+		self.mat.exponent = 100
+		self.trans = None
+	def attach(self, n):
+		m = n.new("material")
+		m.set(self.mat)
+		self.trans = m.new("transform")
+		self.trans.set(ar_TM(self.x))
+		scl = self.trans.new("transform")
+		scl.set(ar_SM(2))
+		s = arCubeMesh()
+		s.attachMesh(scl)
+		#s = arSphereMesh()
+		#s.setAttributes(15)
+		#s.attachMesh(scl)
+	def advance(self):
+		x = self.x[0]
+		y = self.x[1]
+		z = self.x[2]
+		dt = 0.004
+		for i in range(5):
+			x = (10*y - 10*x)*dt + x
+			y = (28*x - y - x*z)*dt + y
+			z = (-(2.66667)*z + x*y)*dt + z
+       	 		#self.x[0] = (10*self.x[1]-10*self.x[0])*self.dt + self.x[0]
+			#self.x[1] = (28*self.x[0]-self.x[1] - self.x[0]*self.x[2])*self.dt + self.x[1]
+			#self.x[2] = (-(2.66667)*self.x[2] + self.x[0]*self.x[1])*self.dt + self.x[2]
+		self.x[0] = x
+		self.x[1] = y
+		self.x[2] = z
+		self.trans.set(ar_TM(self.x))
+
 def addLights(g):
 	r = g.getRoot()
 	l = gcast(r.new("light"))
@@ -19,14 +58,6 @@ def addLights(g):
         light.ambient = arVector3(0,0,0)
         light.diffuse = arVector3(0.5, 0.5, 0.5)
 	l.setLight(light)
-
-def advance(t):
-	dt = 0.004
-	v = ar_ET(t.get())
-        v[0] = (10*v[1]-10*v[0])*dt + v[0]
-	v[1] = (28*v[0]-v[1] - v[0]*v[2])*dt + v[1]
-	v[2] = (-(2.66667)*v[2] + v[0]*v[1])*dt + v[2]
-	t.set(ar_TM(v))
 	
 
 f = arDistSceneGraphFramework()
@@ -40,26 +71,21 @@ f.setNavTransSpeed(0.2)
 g = f.getDatabase()
 addLights(g)
 r = f.getNavNode()
-m = gcast(r.new("material"))
-mat = arMaterial()
-mat.diffuse = arVector3(1,0,0)
-mat.specular = arVector3(0.2, 0.2, 0.2)
-mat.emissive = arVector3(0,0,0.1)
-mat.exponent = 100
-m.set(mat)
-w = m.new("transform")
+w = gcast(r.new("transform"))
 w.set(ar_TM(0,5,-5)*ar_SM(0.1))
-t = w.new("transform","sphere_trans")
-t.set(ar_TM(8, 8, 23))
-s = t.new("transform","sphere_scl")
-s.set(ar_SM(2))
-s = arSphereMesh()
-s.setAttributes(15)
-s.attachMesh("sphere","sphere_scl")
+plist = []
+number = 80
+for i in range(number):
+	p = particle()
+	p.x[2] = p.x[2] + i*0.03
+	p.mat.diffuse = arVector3(1 - (i+1)/(1.0*(number)), 0, (i+1)/(1.0*(number)))
+	p.attach(w)
+	plist.append(p)
 
 while 1:
 	f.setViewer()
 	f.navUpdate()
         f.loadNavMatrix()
-	advance(t)
+	for i in plist:
+		i.advance()
 	f.swapBuffers()
