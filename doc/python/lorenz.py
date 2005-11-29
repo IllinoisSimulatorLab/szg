d879029d2d552899be83527bc93e8b2cb7f71dff
schaeffr <>
2005-11-29 23:55:02 +0000

    continuing to comment the example programs

--- doc/python/lorenz.py
+++ doc/python/lorenz.py
@@ -1,8 +1,14 @@
-# Based on the szglorenz.cpp program of Wayne City High School
+# Based on the szglorenz.cpp program of Wayne City High School.
+# This program demonstrates using the Myriad scene graph, including some of the built-in shapes
+# and line strips. It also shows how to use materials and lighting.
 
+# Import the Syzygy shared libraries.
 from PySZG import *
+# Need the following module for sys.argv, both to parse args AND to pass sys.argv to the framework init.
 import sys
+# Need the following module so we can make the animation framerate independent.
 import time
+# If the user wants cubes as the particle shape, we give them a random angle.
 import random
 
 class particle:
@@ -26,8 +32,11 @@ class particle:
 		self.pointsNode = None
 		# Default material for drawing the object.
 		self.mat = arMaterial()
+		# The default color (for diffuse lighting) is pure red.
 		self.mat.diffuse = arVector3(1,0,0)
+		# By default, a faint white color for specular highlights.
 		self.mat.specular = arVector3(0.2, 0.2, 0.2)
+		# The object, by default, has a very slight green glow.
 		self.mat.emissive = arVector3(0,0,0.1)
 		self.mat.exponent = 100
 		# What shape should we use ('cube' and 'sphere' are valid)?
@@ -122,21 +131,76 @@ class particle:
 # The scene graph needs lights since we are using lighting.
 def addLights(g):
 	r = g.getRoot()
+	# Create a new light node for the scene graph.
 	l = gcast(r.new("light"))
+	# Create a new light description. This will be used to configure the scene graph node.
 	light = arLight()
+	# It is important to give each light a unique ID (0-7).
 	light.lightID = 0
+	# The fourth position being 0 means this is a directional light (as in OpenGL).
+	# If it were 1, this would be a positional light.
 	light.position = arVector4(0,0,-1,0)
+	# Non-directional component of the light (with RGB color components), as in OpenGL.
         light.ambient = arVector3(0,0,0)
+	# The directional component of the light (with RGB color components), as in OpenGL.
         light.diffuse = arVector3(0.5, 0.5, 0.5)
+	# The scene graph node is now configured with the light description.
 	l.setLight(light)
 	l = gcast(r.new("light"))
 	light = arLight()
+	# It is important to give each light a unique ID (0-7). If we repeated an ID,
+	# one of the lights with the same ID would be lost.
 	light.lightID = 1
+	# The fourth position being 0 means this is a directional light (as in OpenGL).
+	# If it were 1, this would be a positional light.
 	light.position = arVector4(0,0,1,0)
         light.ambient = arVector3(0,0,0)
         light.diffuse = arVector3(0.5, 0.5, 0.5)
 	l.setLight(light)
 	
+##########################################################################
+#                           Main Program                                 #
+##########################################################################
+
+# IMPORTANT NOTE ABOUT SYZYGY SCENE GRAPH PROGRAMS: 
+# In "phleet mode", these programs do not have a display themselves, but 
+# instead depend on an external szgrender program.
+# In "standalone mode", these programs do have a display.
+
+# This is a Syzygy "distributed scene graph framework" program.
+# We need a framework object.
+f = arDistSceneGraphFramework()
+
+# Distributed scene graph programs can run in one of two modes:
+#  Automatic graphics buffer swap: In this case, the library
+#   itself decides when the buffer swap should occur.
+#   This is the default.
+#  Manual graphics buffer swap: The programmer is responsible
+#   for requesting a buffer swap. This is better for times
+#   when precise control over the contents of successive frames
+#   is desired.
+# We choose manual swap (by setting auto swap to false). 
+# This must occur BEFORE f.init(...).
+f.setAutoBufferSwap(0)
+
+# This initializes the framework, setting mode of operation and configuration.
+# Configuration information comes from the Phleet (the Syzygy distributed OS).
+# PLEASE NOTE: All Syzygy programs accept a collection of "special" Phleet args
+# (of the form -szg foo=bar). These are stripped from the command line by init.
+# Consequently, the framework init must come BEFORE argument parsing!
+if f.init(sys.argv) != 1:
+	sys.exit()
+
+# Argument parsing. The following are valid invocations of the program.
+#  python lorenz.py
+#    80 particles, no tails, cube shapes
+#  python lorenz.py 200
+#    200 particles, no tails, cube shapes
+#  python lorenz.py 100 1
+#    100 particles. tails, cube shapes
+#  python lorenz.py 500 0 sphere
+#    500 particles, no tails, sphere shapes
+
 if len(sys.argv) > 1:
 	number = int(sys.argv[1])
 else:
@@ -150,33 +214,74 @@ if len(sys.argv) > 3 and sys.argv[3] == "sphere":
 else:
 	shape = "cube"
 
-f = arDistSceneGraphFramework()
-f.setAutoBufferSwap(0)
-if f.init(sys.argv) != 1:
-	sys.exit()
-if f.start() != 1:
-	sys.exit()
-
+# The framework includes a default navigator (implemented by the Syzygy interaction framework).
+# It essentially allows the user to fly around the world along (or transverse) the direction of
+# the wand. The speed is set to 0.2 feet per second.
 f.setNavTransSpeed(0.2)
+# The addLights(...) function expects to be handed an arGraphicsDatabase. We get the
+# database that the framework object holds.
 g = f.getDatabase()
+# The Myriad scene graph uses lighing by default. Consequently, it is VERY important to
+# add lights to it! Otherwise you won't see anything. NOTE: it is actually possible to
+# turn off lighting in a scene graph (or portions thereof). Please see the documentation.
 addLights(g)
+# The navigation node is a special scene graph node created by the framework itself. This
+# node is altered by the framework navigation calls and all scene graph geometry that
+# should be affected by navigation must go below it in the scene graph tree.
+# NOTE: sometimes one does NOT want objects to navigate (like tools that always follow the
+# user). These could be attached, for instance, to the scene graph's root node.
 r = f.getNavNode()
+# We want to put all our objects in the default Syzygy viewing position (center of front
+# CAVE wall, (0,5,-5)) and scale things so that the lorenz attractor is of reasonable size.
+# Create a new node w as a child of the nav node.
 w = gcast(r.new("transform"))
+# ar_TM is an abbreviation for ar_translationMatrix and gives an arMatrix4 with the
+# appropriate translation. ar_SM is an abbreviation for ar_scaleMatrix and gives an 
+# arMatrix4 with the appropriate uniform scaling.
 w.set(ar_TM(0,5,-5)*ar_SM(0.1))
+# Populate the list of particles.
 plist = []
+# The number of particles is determined by the command line args.
 for i in range(number):
 	p = particle()
+	# Set the particle shape based on the command line args.
 	p.type = shape
+	# Whether or not the particle has a tail is determined by the command line args,
 	p.useTail = useTail
+	# The particles are staggered slightly from the default initial position. 
+	# This demonstrated sensitive dependence on initial conditions (one form of chaos).
 	p.x[2] = p.x[2] + i*0.03
+	# It's a better visualization if the particles start out as a rainbow hue.
 	p.mat.diffuse = arVector3(1 - (i+1)/(1.0*(number)), 0, (i+1)/(1.0*(number)))
+	# Add nodes to the scene graph. Recall that the scene graph has a tree structure.
+	# In this case, we are adding the particle under the node w.
 	p.attach(w)
 	plist.append(p)
 
+# Starts all the services, windows, etc. NOTE: it makes the most sense to do
+# this AFTER lengthy initializations of the scene graph (though you can issue
+# this statement any time after initializing the framework).
+if f.start() != 1:
+	sys.exit()
+
+# Main loop.
 while 1:
+	# Get the user head position/orientation from control device and post to
+	# the framework. By making this step explicit, we allow greater user control.
+	# Without this statement, the scene will not change in response to head
+	# position changes!
 	f.setViewer()
+	# Update the framework's navigation matrix. This processes user input and
+	# changes the navigation matrix held inside the framework (but does not
+	# update the scene graph yet). Without this call, no navigation will occur!
 	f.navUpdate()
+	# This actually loads the navigation matrix into the scene graph. Without this
+	# call, no navigation will occur!
         f.loadNavMatrix()
+	# Go through the list of particles, updating their positions, trails (if selected),
+	# and graphical representation in the scene graph.
 	for i in plist:
 		i.advance()
+	# We have chosen the manual swap mode of operation for the scene graph framework.
+	# Consequently, we must issue the buffer swap command ourselves.
 	f.swapBuffers()
