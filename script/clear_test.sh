026e77d98cddd5b18f7e59731fe4b609748a62c5
schaeffr <>
2005-12-30 05:13:52 +0000

    foo

---
+++ script/clear_test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+export PS1=CLEAR:
+export SZG_RENDER_texture_path=NULL
+export SZG_RENDER_text_path=NULL
+export SZG_PEER_path=NULL
+export SZG_SOUND_path=NULL
+export SZG_PARAM=NULL
+export SZG_DISPLAY0_name=NULL
+export SZG_DISPLAY1_name=NULL
+export SZG_DISPLAY2_name=NULL
+export SZG_DISPLAY3_name=NULL
--- script/ezszg.sh
+++ script/ezszg.sh
@@ -1,10 +1,29 @@
 #!/bin/sh
-if [ "$SHELL" = "/bin/bash" ]; then
-  echo "Remark: assuming using bash shell."
-  # Make a special szg prompt.
+# ONLY WORKS FOR MAC OS X SO FAR!
+# Make a special szg prompt.
+TEMP=$PWD/doc/images/NormalAl.jpg
+# Check to see if we are in the right spot... Necessary since all variables
+# are relative to the current directory.
+if [ -f $TEMP ]; then
+  echo "Setting up your szg SDK..."
+  # TODO: Check to make sure this script has not been executed twice.
   export PS1=SZG:\\u@\\h:\\w\\r\\n::
+  export PATH=$PWD/bin:$PATH
+  export PYTHONPATH=$PWD/bin:$PYTHONPATH
+  export DYLD_LIBRARY_PATH=$PWD/bin:$DYLD_LIBRARY_PATH
+  export SZG_RENDER_texture_path=$PWD/rsc
+  export SZG_RENDER_text_path=$PWD/rsc
+  export SZG_PEER_path=$PWD/rsc
+  export SZG_SOUND_path=$PWD/rsc
+  export SZG_PARAM=$PWD/doc/window_configs.txt
+  export SZG_DISPLAY0_name=single_window
+  export SZG_DISPLAY1_name=virtual_cube_window
+  export SZG_DISPLAY2_name=quad_window
+  export SZG_DISPLAY3_name=single_window_fullscreen
 else
-  # Assume this is tsch and use that syntax.
-  setenv PS1 SZG:\\u@\\h:\\w\\r\\n::
+  echo "This script must be run from the top-level of your szg SDK directory."
 fi
+
+
+
 
