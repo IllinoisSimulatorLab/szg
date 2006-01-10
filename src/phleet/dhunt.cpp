e2b50b444a7e32e8b17e608829c2fe6edc4f9a15
camilleg <>
2006-01-10 01:48:17 +0000

    comments, consts

--- src/phleet/dhunt.cpp
+++ src/phleet/dhunt.cpp
@@ -24,31 +24,33 @@ int main(int argc, char** argv){
   // we send out a broadcast packet on each interface in turn
   arSZGClient client;
   // The discovery threads should be launched by us now, as opposed to
-  // implicitly by the discoverSZGServer function. Thus, if we get an error,
-  // then we can bail out with a reasonable error message.
+  // implicitly by discoverSZGServer(). Thus, if we get an error,
+  // we can bail out with a reasonable error message.
   if (!client.launchDiscoveryThreads()){
-    // Upon error, a complaint has already been logged.
+    // A complaint has already been logged.
     return 1;
   }
   const arSlashString networkList(parser.getNetworks());
   const arSlashString addressList(parser.getAddresses());
   const arSlashString maskList(parser.getMasks());
   const int numNetworks = networkList.size();
+  //;;;; Could we spawn threads to broadcast on all subnets at once?
   for (int i=0; i<numNetworks; i++){
-    const string address = addressList[i];
-    const string mask = maskList[i];
+    const string address(addressList[i]);
+    const string mask(maskList[i]);
     arSocketAddress tmpAddress;
     if (!tmpAddress.setAddress(address.c_str(), 0)){
       cout << "dhunt remark: illegal address (" << address
 	   << ") in szg.conf.\n";
       continue;
     }
-    string broadcast = tmpAddress.broadcastAddress(mask.c_str());
+    const string broadcast(tmpAddress.broadcastAddress(mask.c_str()));
     if (broadcast == "NULL"){
       cout << "dhunt remark: illegal mask ("
 	   << mask << ") for address (" << address << ").\n";
       continue;
     }
+    //;;;; only say Broadcasting on, if -v flag
     cout << "Broadcasting on " << broadcast << "\n";
     client.printSZGServers(broadcast);
   }
