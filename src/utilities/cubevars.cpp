e13814cf3ac143c8d1be27ecc4dfac2ff8094241
camilleg <>
2006-06-08 02:56:34 +0000

    arbitrary number of buttons, axes, matrices

--- src/utilities/cubevars.cpp
+++ src/utilities/cubevars.cpp
@@ -14,7 +14,7 @@
 
 int billboardID = -1;
 
-bool setUpWorld(arMasterSlaveFramework&, arSZGClient&){
+bool callbackStart(arMasterSlaveFramework&, arSZGClient&){
   arCubeEnvironment bigCube;
   bigCube.setHeight(20);
   bigCube.setRadius(14.14);
@@ -38,15 +38,58 @@ bool setUpWorld(arMasterSlaveFramework&, arSZGClient&){
   return true;
 }
 
-void drawCallback(arMasterSlaveFramework& fw){
-  const arMatrix4 headMatrix = fw.getMatrix(0);
-  const arMatrix4 wandMatrix = fw.getMatrix(1);
+void drawWand(const arMatrix4& m) {
+  // Wand display
+  glPushMatrix();
+    glMultMatrixf(m.v);
+    glBegin(GL_LINES);
+      glColor3f(1,1,0);
+        glVertex3f(0,0,1);
+        glVertex3f(0,0,-1);
+      glColor3f(0,1,1);
+        glVertex3f(1,0,0);
+        glVertex3f(-1,0,0);
+      glColor3f(1,0,1);
+        glVertex3f(0,1,0);
+        glVertex3f(0,-1,0);
+    glEnd();
+    glPushMatrix();
+      glTranslatef(0,0,-1);
+      glColor3f(1,0,1);
+      glutSolidSphere(0.07,10,10);
+    glPopMatrix();
+  glPopMatrix();
+}
+
+void callbackDraw(arMasterSlaveFramework& fw){
+  int i;
+
+  arMatrix4 rgm[20]; // buffer overflow
+  const unsigned cm = fw.getNumberMatrices();
+  if (cm < 2)
+    cerr << "warning: expect at least a head and wand matrix.\n";
+  if (cm > 20)
+    cerr << "warning: too many matrices.\n";
+  for (i=0; i<cm; ++i)
+    rgm[i] = fw.getMatrix(i);
+  const arMatrix4& headMatrix = rgm[0];
+  const arMatrix4& wandMatrix = rgm[1];
+
+  const unsigned ca = fw.getNumberAxes();
   const float caveJoystickX = fw.getAxis(0);
   const float caveJoystickY = fw.getAxis(1);
-  const int caveButton[3] =
-    { fw.getButton(0), fw.getButton(1), fw.getButton(2) };
 
-  // draw wireframe cubes -- these should not translate, as head translates!
+  const unsigned cb = fw.getNumberButtons();
+  if (cb < 3)
+    cerr << "warning: expect at least 3 buttons.\n";
+  if (cb > 250)
+    cerr << "warning: too many buttons.\n";
+  int rgButton[257];
+  for (i=0; i < cb; ++i)
+    rgButton[i] = fw.getButton(i);
+
+
+  // Wireframe cubes -- these should not translate, as head translates.
   glDisable(GL_LIGHTING);
   glColor3f(1,1,1);
   glPushMatrix();
@@ -60,7 +103,7 @@ void drawCallback(arMasterSlaveFramework& fw){
   glEnable(GL_DEPTH_TEST);
   glLineWidth(3);
 
-  // draw the head display, to ensure orientation is correct!
+  // Head display, to verify orientation.
   glPushMatrix();
     glMultMatrixf(headMatrix.v);
     glTranslatef(0,0,-2); // in front of your eyes
@@ -82,77 +125,53 @@ void drawCallback(arMasterSlaveFramework& fw){
     glPopMatrix();
   glPopMatrix();
 
-  // draw the wand display
-  glPushMatrix();
-    glMultMatrixf(wandMatrix.v);
-    glBegin(GL_LINES);
-      glColor3f(1,1,0);
-        glVertex3f(0,0,1);
-        glVertex3f(0,0,-1);
-      glColor3f(0,1,1);
-        glVertex3f(1,0,0);
-        glVertex3f(-1,0,0);
-      glColor3f(1,0,1);
-        glVertex3f(0,1,0);
-        glVertex3f(0,-1,0);
-    glEnd();
-    glPushMatrix();
-      glTranslatef(0,0,-1);
-      glColor3f(1,0,1);
-      glutSolidSphere(0.07,10,10);
-    glPopMatrix();
-  glPopMatrix();
+  // Wand(s)
+  for (i=1; i<cm; ++i)
+    drawWand(rgm[i]);
 
   glLineWidth(1);
 
   const arMatrix4 m(ar_extractTranslationMatrix(wandMatrix));
   glMultMatrixf(m.v);
-  // one cube for the wand's joystick, in a square
-  glColor3f(0.3,1,0);
+
+  // Box for the wand's joystick, in a square.
   glPushMatrix();
     glTranslatef(0.5 ,-0.6, 0);
     glScalef(.3, .3, .3);
+    glColor3f(1,1,.3);
     glBegin(GL_LINE_LOOP);
       glVertex3f( 1,  1, 0);
       glVertex3f( 1, -1, 0);
       glVertex3f(-1, -1, 0);
       glVertex3f(-1,  1, 0);
     glEnd();
+    glBegin(GL_LINES);
+      glVertex3f( 0,  1, 0);
+      glVertex3f( 0, -1, 0);
+      glVertex3f( 1,  0, 0);
+      glVertex3f(-1,  0, 0);
+    glEnd();
+    glColor3f(0.3,.9,0);
     glPushMatrix();
       glTranslatef(caveJoystickX, caveJoystickY, 0);
       glutSolidCube(0.35);
     glPopMatrix();
   glPopMatrix();
-  // three cubes for the three wand buttons
-  glPushMatrix();
-    glTranslatef(0.1,-0.2,0);
-    if (caveButton[0] == 0)
-      glColor3f(1,0,0);
-    else
-      glColor3f(0,1,0);
-    glutSolidCube(0.1);
-  glPopMatrix();
-  glPushMatrix();
-    glTranslatef(0.5,-0.2,0);
-    if (caveButton[1] == 0)
-      glColor3f(1,0,0);
-    else
-      glColor3f(0,1,0);
-    glutSolidCube(0.1);
-  glPopMatrix();
-  glPushMatrix();
-    glTranslatef(0.9,-0.2,0);
-    if (caveButton[2] == 0)
-      glColor3f(1,0,0);
-    else
-      glColor3f(0,1,0);
-    glutSolidCube(0.1);
-  glPopMatrix();
-  glEnable(GL_LIGHTING);
 
-  // and finally, draw the framework itself.
-  // DEPRECATED in this use, re-purposed by arGUI
-  // fw.draw();
+  // Box for each wand button
+  for (i=0; i<cb; ++i) {
+    glPushMatrix();
+      const float x = 0.1 + 0.8 * (i / (cb - 1.0));
+      glTranslatef(x, -0.2, 0);
+      if (rgButton[i] == 0)
+	glColor3f(1,0,0);
+      else
+	glColor3f(0,1,0);
+      glutSolidCube(0.5 / cb);
+    glPopMatrix();
+  }
+
+  glEnable(GL_LIGHTING);
 }
 
 int main(int argc, char** argv){
@@ -160,8 +179,8 @@ int main(int argc, char** argv){
   if (!framework.init(argc, argv))
     return 1;
 
-  framework.setStartCallback(setUpWorld);
-  framework.setDrawCallback(drawCallback);
+  framework.setStartCallback(callbackStart);
+  framework.setDrawCallback(callbackDraw);
   framework.setClipPlanes( .1, 300. );
   return framework.start() ? 0 : 1;
 }
