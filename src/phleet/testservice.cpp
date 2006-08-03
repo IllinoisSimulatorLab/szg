a3af49be24e757dbcbd3f02eee1ae2b0b72d4b4d
camilleg <>
2006-08-03 02:51:36 +0000

    diagnostics

--- src/phleet/testservice.cpp
+++ src/phleet/testservice.cpp
@@ -18,37 +18,37 @@ void connectionTask(void*){
 }
 
 int main(int argc, char** argv){
-  if (argc < 2){
-    cout << "usage: testservice <service name>\n";
-    return 1;
-  }
-
   arSZGClient client;
   client.init(argc, argv);
   if (!client){
     return 1;
   }
 
+  if (argc < 2){
+    cout << "usage: testservice serviceName\n";
+    return 1;
+  }
+
   // Create the language.
   arTemplateDictionary dictionary;
   arDataTemplate record("test");
-  int TAG = record.add("tag", AR_CHAR);
+  const int TAG = record.add("tag", AR_CHAR);
   record.add("data", AR_INT);
   dictionary.add(&record);
   int ports[10];
   if (!client.registerService(argv[1],"default",1,ports)){
-    cout << "testservice remark: failed to register service.\n";
-    cout << "  Will try one more time upon current service expiration.\n";
-    int match = client.requestServiceReleaseNotification(argv[1]);
+    ar_log_remark() << "testservice failed to register service.  Will retry when current service expires.\n";
+    const int match = client.requestServiceReleaseNotification(argv[1]);
     list<int> tags;
     tags.push_back(match);
     if (client.getServiceReleaseNotification(tags) < 0){
-      cout << "szgserver error: failed to get matching tag.\n";
+      ar_log_error() << "testservice failed to get matching tag.\n";
       return 1;
     }
-    cout << "Old service has expired. Attempting to register again.\n";
+
+    ar_log_remark() << "testservice: Old service expired. Re-registering.\n";
     if (!client.registerService(argv[1],"default",1,ports)){
-      cout << "testservice error: failed again to register servie.\n";
+      ar_log_error() << "testservice failed twice register service.\n";
       return 1;
     }
   }
@@ -58,25 +58,24 @@ int main(int argc, char** argv){
   int tries = 0;
   while (!server.beginListening(&dictionary)){
     if (!client.requestNewPorts(argv[1],"default",1,ports)){
-      cout << "testservice error: failed to request ports.\n";
+      ar_log_error() << "testservice failed to request ports.\n";
       return 1;
     }
+
     server.setPort(ports[0]);
     if (++tries == 10){
-      cout << "testservice error: too many port request attempts.\n";
+      ar_log_error() << "testservice requested ports too many times.\n";
       return 1;
     }
   }
 
   if (!client.confirmPorts(argv[1],"default",1,ports)){
-    cout << "testservice error: failed to confirm ports.\n";
+    ar_log_error() << "testservice failed to confirm ports.\n";
     return 1;
   }
 
-  cout << "testservice remark: successfully bound to brokered ports.\n";
-  arThread connectionThread;
-  connectionThread.beginThread(connectionTask, NULL);
-
+  ar_log_remark() << "testservice bound to brokered ports.\n";
+  arThread dummy(connectionTask);
   arStructuredData data(&record);
   int counter = 0;
   while (true){
