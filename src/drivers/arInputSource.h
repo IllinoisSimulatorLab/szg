06ea8bdb7830b2bc27e3590139fb9ca2cf4af150
camilleg <>
2007-02-17 03:37:50 +0000

    typo

--- src/drivers/arInputSource.h
+++ src/drivers/arInputSource.h
@@ -87,7 +87,7 @@ class SZG_CALL arInputSource{
   // init() calls this to set the device signature.
   void _setDeviceElements(int buttons, int axes, int matrices);
   void _setDeviceElements(const ARint* nums)
-    { return _setDeviceElements(nums[0], nums[1], nums[2]); }
+    { _setDeviceElements(nums[0], nums[1], nums[2]); }
 
   bool _fillCommonData(arStructuredData*);
 
