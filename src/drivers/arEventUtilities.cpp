8be7766d2125271b397ef0609592a49565a12ef3
camilleg <>
2007-05-09 04:00:36 +0000

    decopypasting

--- src/drivers/arEventUtilities.cpp
+++ src/drivers/arEventUtilities.cpp
@@ -11,62 +11,58 @@ static inline int maxint(const unsigned a, const int b) { return int(a)>b ? int(
 static inline int maxint(const int a, const unsigned b) { return a>int(b) ? a : int(b); }
 static inline int maxint(const unsigned a, const unsigned b) { return int(a>b ? a : b); }
 
-static inline int maxint( const int a, const int b, const int c ) {
-  return maxint(a, maxint(b, c));
-}
+// todo: ar_toStructuredData like ar_fromStructuredData, to decopypaste.
 
-// todo: remove 60-line copypaste with ar_setInputStateFromStructuredData
+bool ar_fromStructuredData(int& sigLen, int& sigField, int* sigBuf,
+         int*& typeBuf, int*& indexBuf, int*& buttonBuf, float*& axisBuf, float*& matrixBuf,
+	 int& numItems, int& numButtons, int& numAxes, int& numMatrices,
+         const arStructuredData* data) {
 
-bool ar_setEventQueueFromStructuredData( arInputEventQueue* q,
-                                         const arStructuredData* data ) {
-  const int sigField = data->getDataFieldIndex("signature");
-  const int typeField = data->getDataFieldIndex("types");
-  const int indexField = data->getDataFieldIndex("indices");
+               sigField = data->getDataFieldIndex("signature");
+  const int   typeField = data->getDataFieldIndex("types");
+  const int  indexField = data->getDataFieldIndex("indices");
   const int buttonField = data->getDataFieldIndex("buttons");
-  const int axisField = data->getDataFieldIndex("axes");
+  const int   axisField = data->getDataFieldIndex("axes");
   const int matrixField = data->getDataFieldIndex("matrices");
 
-  const int sigLen      = data->getDataDimension(sigField);
-  const int numItems    = data->getDataDimension(typeField);
+            sigLen      = data->getDataDimension(sigField);
+            numItems    = data->getDataDimension(typeField);
   const int numIndices  = data->getDataDimension(indexField);
-  const int numButtons  = data->getDataDimension(buttonField);
-  const int numAxes     = data->getDataDimension(axisField);
-        int numMatrices = data->getDataDimension(matrixField);
+            numButtons  = data->getDataDimension(buttonField);
+            numAxes     = data->getDataDimension(axisField);
+            numMatrices = data->getDataDimension(matrixField);
 
   if (sigLen != 3)
-    ar_log_warning() << "ar_setEventQueueFromStructuredData: invalid signature.\n";
+    ar_log_warning() << "ar_fromStructuredData: invalid signature.\n";
 
-  if (numMatrices % 16 != 0) {
-    ar_log_warning() << "ar_setEventQueueFromStructuredData got a fractional number"
-         << " of matrices (" << numMatrices << "/16).\n";
+  if (numMatrices % 16 != 0){
+    ar_log_warning() << "ar_fromStructuredData: fractional number of matrices (" <<
+      numMatrices << "/16).\n";
     return false;
   }
+
   numMatrices /= 16;
-  if (numItems != numIndices) {
-    ar_log_warning() << "ar_setEventQueueFromStructuredData dictionary "
-         << " got different number of types and indices,\n\t"
-         << numItems << " and " << numIndices
-         << " respectively.  Ignoring data packet.\n";
-    return false;
-  }
-  if (numButtons + numAxes + numMatrices != numItems) {
-    ar_log_warning() << "ar_setEventQueueFromStructuredData dictionary "
-         << " got a count mismatch:\n\t"
-         << numButtons <<"+"<< numAxes <<"+"<< numMatrices
-         << " doesn't equal "
-         << numItems << ".  Ignoring data packet.\n";
+  if (numItems != numIndices){
+    ar_log_warning() << "ar_fromStructuredData mismatch: " <<
+      numItems << " types but " << numIndices << " indices.\n";
     return false;
   }
 
-  int sigBuf[3];
-  int* typeBuf = new int[numItems];
-  int* indexBuf = new int[numItems];
-  int* buttonBuf = new int[numButtons];
-  float* axisBuf = new float[numAxes];
-  float* matrixBuf = new float[16*numMatrices];
+  if (numButtons + numAxes + numMatrices != numItems){
+    ar_log_warning() << "ar_fromStructuredData dictionary mismatch: " <<
+      numButtons <<"+"<< numAxes <<"+"<< numMatrices << " != " << numItems << ".\n";
+    return false;
+  }
+
+  // Caller's responsible for delete[]'ing these.
+  typeBuf = new int[numItems];
+  indexBuf = new int[numItems];
+  buttonBuf = new int[numButtons];
+  axisBuf = new float[numAxes];
+  matrixBuf = new float[16*numMatrices];
 
   if (!typeBuf || !indexBuf || !buttonBuf || !axisBuf || !matrixBuf) {
-    ar_log_warning() << "ar_setEventQueueFromStructuredData out of memory.\n";
+    ar_log_warning() << "ar_fromStructuredData out of memory.\n";
     return false;
   }
 
@@ -76,27 +72,53 @@ bool ar_setEventQueueFromStructuredData( arInputEventQueue* q,
   data->dataOut( axisField, axisBuf, AR_FLOAT, numAxes );
   data->dataOut( matrixField, matrixBuf, AR_FLOAT, numMatrices*16 );
 
+  unsigned i;
   if (sigLen == 3) {
     data->dataOut( sigField, sigBuf, AR_INT, sigLen );
-    for (int i=0; i<sigLen; i++) {
+    for (i=0; i<(unsigned)sigLen; i++) {
       if (sigBuf[i] < 0) {
-        ar_log_warning() << "ar_setEventQueueFromStructuredData zeroing negative signature.\n";
+        ar_log_warning() << "ar_fromStructuredData zeroing received negative signature.\n";
         sigBuf[i] = 0;
       }
     }
+  }
+
+  return true;
+}
+
+void ar_fromStructuredDataEnd(int*& typeBuf, int*& indexBuf, int*& buttonBuf, float*& axisBuf, float*& matrixBuf) {
+  delete [] typeBuf;
+  delete [] indexBuf;
+  delete [] buttonBuf;
+  delete [] axisBuf;
+  delete [] matrixBuf;
+}
+
+bool ar_setEventQueueFromStructuredData( arInputEventQueue* q,
+                                         const arStructuredData* data ) {
+  int sigLen, sigField, sigBuf[3];
+  int* typeBuf;
+  int* indexBuf;
+  int* buttonBuf;
+  float* axisBuf;
+  float* matrixBuf;
+  int numItems, numButtons, numAxes, numMatrices;
+  if (!ar_fromStructuredData(sigLen, sigField, sigBuf,
+      typeBuf, indexBuf, buttonBuf, axisBuf, matrixBuf,
+      numItems, numButtons, numAxes, numMatrices,
+      data))
+    return false;
+
+  if (sigLen == 3) {
     sigBuf[0] = maxint( sigBuf[0], numButtons );
     sigBuf[1] = maxint( sigBuf[1], numAxes );
     sigBuf[2] = maxint( sigBuf[2], numMatrices );
     q->setSignature( (unsigned)sigBuf[0], (unsigned)sigBuf[1], (unsigned)sigBuf[2] );
   }
 
-  const bool ok = q->setFromBuffers( typeBuf, indexBuf, buttonBuf, numButtons,
-                                   axisBuf, numAxes, matrixBuf, numMatrices );
-  delete [] typeBuf;
-  delete [] indexBuf;
-  delete [] buttonBuf;
-  delete [] axisBuf;
-  delete [] matrixBuf;
+  const bool ok = q->setFromBuffers(
+    typeBuf, indexBuf, buttonBuf, numButtons, axisBuf, numAxes, matrixBuf, numMatrices );
+  ar_fromStructuredDataEnd(typeBuf, indexBuf, buttonBuf, axisBuf, matrixBuf);
   return ok;
 }
 
@@ -118,6 +140,9 @@ bool ar_saveEventQueueToStructuredData( const arInputEventQueue* q,
   const int _numAxes = q->getNumberAxes();
   const int _numMatrices = q->getNumberMatrices();
   const int _numItems = _numButtons + _numAxes + _numMatrices;
+  if (_numItems != int(q->size()))
+    ar_log_warning() << "ar_saveEventQueueToStructuredData: queue miscount, " <<
+      _numButtons << "+" << _numAxes << "+" << _numMatrices << " != " << q->size() << ".\n";
 
   if (numButtons != _numButtons)
     if (!data->setDataDimension( buttonField, _numButtons )) {
@@ -150,23 +175,32 @@ bool ar_saveEventQueueToStructuredData( const arInputEventQueue* q,
       return false;
     }
 
-  const int sigBuf[] = {
-    q->getButtonSignature(),
-    q->getAxisSignature(), 
-    q->getMatrixSignature() };
   int* typeBuf = new int[_numItems];
   int* indexBuf = new int[_numItems];
   int* buttonBuf = new int[_numButtons];
   float* axisBuf = new float[_numAxes];
   float* matrixBuf = new float[16*_numMatrices];
 
-  const bool ok = q->saveToBuffers( typeBuf, indexBuf, buttonBuf, axisBuf, matrixBuf );
+  if (!typeBuf || !indexBuf || !buttonBuf || !axisBuf || !matrixBuf) {
+    ar_log_warning() << "ar_setEventQueueToStructuredData out of memory.\n";
+    return false;
+  }
+
+  const int sigBuf[] = {
+    q->getButtonSignature(),
+    q->getAxisSignature(), 
+    q->getMatrixSignature() };
+  // ar_log_critical() << "packing sig " << sigBuf[0] << sigBuf[1] << sigBuf[2] << ".\n";
   data->dataIn( "signature", sigBuf, AR_INT, 3 );
-  data->dataIn( typeField, typeBuf, AR_INT, _numItems );
-  data->dataIn( indexField, indexBuf, AR_INT, _numItems );
-  data->dataIn( buttonField, buttonBuf, AR_INT, _numButtons );
-  data->dataIn( axisField, axisBuf, AR_FLOAT, _numAxes );
-  data->dataIn( matrixField, matrixBuf, AR_FLOAT, _numMatrices*16 );
+
+  const bool ok = q->saveToBuffers( typeBuf, indexBuf, buttonBuf, axisBuf, matrixBuf );
+  if (ok) {
+    data->dataIn( typeField, typeBuf, AR_INT, _numItems );
+    data->dataIn( indexField, indexBuf, AR_INT, _numItems );
+    data->dataIn( buttonField, buttonBuf, AR_INT, _numButtons );
+    data->dataIn( axisField, axisBuf, AR_FLOAT, _numAxes );
+    data->dataIn( matrixField, matrixBuf, AR_FLOAT, _numMatrices*16 );
+  }
   delete [] typeBuf;
   delete [] indexBuf;
   delete [] buttonBuf;
@@ -175,107 +209,46 @@ bool ar_saveEventQueueToStructuredData( const arInputEventQueue* q,
   return ok;
 }
 
-// todo: remove 60-line copypaste with ar_setEventQueueFromStructuredData
-
 bool ar_setInputStateFromStructuredData( arInputState* state,
                                          const arStructuredData* data ) {
-  const int sigField = data->getDataFieldIndex("signature");
-  const int typeField = data->getDataFieldIndex("types");
-  const int indexField = data->getDataFieldIndex("indices");
-  const int buttonField = data->getDataFieldIndex("buttons");
-  const int axisField = data->getDataFieldIndex("axes");
-  const int matrixField = data->getDataFieldIndex("matrices");
-
-  const int sigLen      = data->getDataDimension(sigField);
-  const int numItems    = data->getDataDimension(typeField);
-  const int numIndices  = data->getDataDimension(indexField);
-  const int numButtons  = data->getDataDimension(buttonField);
-  const int numAxes     = data->getDataDimension(axisField);
-        int numMatrices = data->getDataDimension(matrixField);
-
-  if (sigLen != 3)
-    ar_log_warning() << "ar_setInputStateFromStructuredData: invalid signature.\n";
-
-  if (numMatrices % 16 != 0){
-    ar_log_warning() << "ar_setInputStateFromStructuredData got a fractional number"
-         << " of matrices (" << numMatrices << "/16).\n";
+  int sigLen, sigField, sigBuf[3];
+  int* typeBuf;
+  int* indexBuf;
+  int* buttonBuf;
+  float* axisBuf;
+  float* matrixBuf;
+  int numItems, numButtons, numAxes, numMatrices;
+  if (!ar_fromStructuredData(sigLen, sigField, sigBuf,
+      typeBuf, indexBuf, buttonBuf, axisBuf, matrixBuf,
+      numItems, numButtons, numAxes, numMatrices,
+      data))
     return false;
-  }
-  numMatrices /= 16;
-  if (numItems != numIndices){
-    ar_log_warning() << "ar_setInputStateFromStructuredData dictionary "
-         << " got different number of types and indices,\n\t"
-         << numItems << " and " << numIndices
-         << " respectively.  Ignoring data packet.\n";
-    return false;
-  }
-  if (numButtons + numAxes + numMatrices != numItems){
-    ar_log_warning() << "ar_setInputStateFromStructuredData dictionary "
-         << " got a count mismatch:\n\t"
-         << numButtons <<"+"<< numAxes <<"+"<< numMatrices
-         << " doesn't equal "
-         << numItems << ".  Ignoring data packet.\n";
-    return false;
-  }
-
-  int* typeBuf = new int[numItems];
-  int* indexBuf = new int[numItems];
-  int* buttonBuf = new int[numButtons];
-  float* axisBuf = new float[numAxes];
-  float* matrixBuf = new float[16*numMatrices];
-
-  if (!typeBuf || !indexBuf || !buttonBuf || !axisBuf || !matrixBuf) {
-    ar_log_warning() << "ar_setInputStateFromStructuredData out of memory.\n";
-    return false;
-  }
-
-  data->dataOut( typeField, typeBuf, AR_INT, numItems );
-  data->dataOut( indexField, indexBuf, AR_INT, numItems );
-  data->dataOut( buttonField, buttonBuf, AR_INT, numButtons );
-  data->dataOut( axisField, axisBuf, AR_FLOAT, numAxes );
-  data->dataOut( matrixField, matrixBuf, AR_FLOAT, numMatrices*16 );
-
-  unsigned i;
-  int sigBuf[3];
-  if (sigLen == 3) {
-    data->dataOut( sigField, sigBuf, AR_INT, sigLen );
-    for (i=0; i<(unsigned)sigLen; i++) {
-      if (sigBuf[i] < 0) {
-        ar_log_warning() << "ar_setEventQueueFromStructuredData overriding negative signature.\n";
-        sigBuf[i] = 0;
-      }
-    }
-  }
 
   // Compute signature from max of eventIndex.
-  unsigned buttonSig = 0;
-  unsigned axisSig = 0;
-  unsigned matrixSig = 0;
+  unsigned i;
+  unsigned stateSig[3] = {0};
   for (i=0; i<(unsigned)numItems; i++) {
     const unsigned eventIndex = unsigned(indexBuf[i]);
     switch (typeBuf[i]) {
+      case AR_EVENT_GARBAGE:
+        break;
       case AR_EVENT_BUTTON:
-        if (eventIndex >= buttonSig)
-          buttonSig = eventIndex + 1;
-        break;
       case AR_EVENT_AXIS:
-        if (eventIndex >= axisSig)
-          axisSig = eventIndex + 1;
-        break;
       case AR_EVENT_MATRIX:
-        if (eventIndex >= matrixSig)
-          matrixSig = eventIndex + 1;
+        unsigned& sig = stateSig[typeBuf[i]];
+	if (eventIndex >= sig)
+          sig = eventIndex + 1;
         break;
     }
   }
 
-  // buttonSig, axisSig, and matrixSig are not maxed with
-  // the current number of buttons, axes, and matrices in the input state,
-  // so that a disconnected input device can *decrease* its signature.
-  buttonSig = (unsigned)maxint( buttonSig, sigBuf[0]);
-  axisSig = (unsigned)maxint( axisSig, sigBuf[1]);
-  matrixSig = (unsigned)maxint( matrixSig, sigBuf[2]);
-  state->setSignature( buttonSig, axisSig, matrixSig );
+  // Do not max stateSig with the
+  // number of buttons, axes, and matrices of the **input state**.
+  // This lets a disconnected input device *decrease* its signature.
+
+  for (i=0; i<3; ++i)
+    stateSig[i] = (unsigned)maxint( stateSig[i], sigBuf[i]);
+  state->setSignature( stateSig[0], stateSig[1], stateSig[2] );
 
   bool ok = true;
   unsigned iButton = 0;
@@ -318,11 +291,7 @@ bool ar_setInputStateFromStructuredData( arInputState* state,
     }
   }
 
-  delete [] typeBuf;
-  delete [] indexBuf;
-  delete [] buttonBuf;
-  delete [] axisBuf;
-  delete [] matrixBuf;
+  ar_fromStructuredDataEnd(typeBuf, indexBuf, buttonBuf, axisBuf, matrixBuf);
   return ok;
 }
 
