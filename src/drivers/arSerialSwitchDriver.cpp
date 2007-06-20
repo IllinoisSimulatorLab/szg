dc340956ba2c275f3f79ce2d20cc691ceb7c6c60
camilleg <>
2007-06-20 02:03:25 +0000

    cleanup

--- src/drivers/arSerialSwitchDriver.cpp
+++ src/drivers/arSerialSwitchDriver.cpp
@@ -26,18 +26,18 @@ bool arSerialSwitchDriver::init(arSZGClient& SZGClient){
 
   // Determine the baud rate.
   const int baudRates[] = {2400,4800,9600,19200,38400,57600,115200};
-  const int NUM_BAUD_RATES(7);
-  bool baudRateFound = false;
+  const int NUM_BAUD_RATES = sizeof(baudRates) / sizeof(int);
+  bool fFound = false;
   int baudRate = SZGClient.getAttributeInt("SZG_SERIALSWITCH", "baud_rate");
   if (baudRate == 0)
     goto LDefaultBaudRate;
   for (i=0; i<NUM_BAUD_RATES; ++i){
     if (baudRate == baudRates[i]) {
-      baudRateFound = true;
+      fFound = true;
       break;
     }
   }
-  if (!baudRateFound) {
+  if (!fFound) {
     cerr << "arSerialSwitchDriver got unexpected SZG_SERIALSWITCH/baud_rate "
 	 << baudRate << ".\n  Expected one of:";
     for (i=0; i<NUM_BAUD_RATES; ++i)
@@ -67,23 +67,19 @@ LDefaultBaudRate:
     return false;
   }
 
-  string eventType = SZGClient.getAttribute( "SZG_SERIALSWITCH", "event_type", "|both|open|closed|" );
-  if (eventType == "closed") {
-    _eventType = AR_CLOSED_SWITCH_EVENT;
-  } else if (eventType == "open") {
-    _eventType = AR_OPEN_SWITCH_EVENT;
-  } else {
-    _eventType = AR_BOTH_SWITCH_EVENT;
-  }
+  const string eventType = SZGClient.getAttribute( "SZG_SERIALSWITCH", "event_type", "|both|open|closed|" );
+  _eventType = eventType == "closed" ? AR_CLOSED_SWITCH_EVENT :
+	       eventType == "open"   ? AR_OPEN_SWITCH_EVENT :
+	       AR_BOTH_SWITCH_EVENT;
 
   int signalByte = SZGClient.getAttributeInt( "SZG_SERIALSWITCH", "signal_byte" );
   if (signalByte == 0) {
-    ar_log_warning() << "SZG_SERIALSWITCH/signal_byte not set or set to 0, using default ("
-                     << (int)DEFAULT_SIGNAL << ").\n";
+    ar_log_warning() << "SZG_SERIALSWITCH/signal_byte missing or 0, defaulting to " <<
+      (int)DEFAULT_SIGNAL << ".\n";
     signalByte = DEFAULT_SIGNAL;
   } else if (signalByte > 255) {
-    ar_log_error() << "SZG_SERIALSWITCH/signal_byte must be < 256, using default ("
-                     << (int)DEFAULT_SIGNAL << ") instead.\n";
+    ar_log_warning() << "SZG_SERIALSWITCH/signal_byte > 255, defaulting to " <<
+      (int)DEFAULT_SIGNAL << ".\n";
     signalByte = DEFAULT_SIGNAL;
   }
   _outChar = (char)signalByte;
@@ -91,7 +87,7 @@ LDefaultBaudRate:
   // Report one axis, the time since the last event occurred.
   _setDeviceElements( 0, 1, 0 );
 
-  cerr << "arSerialSwitchDriver inited().\n";
+  ar_log_debug() << "arSerialSwitchDriver inited.\n";
   return true;
 }
 
@@ -103,19 +99,16 @@ void ar_SerialSwitchDriverEventTask(void* driver){
   ((arSerialSwitchDriver*)driver)->_eventloop();
 }
 
-void arSerialSwitchDriver::_eventloop(){
+void arSerialSwitchDriver::_eventloop() {
   _eventThreadRunning = true;
-  for (;;) {
+  while (!_stopped) {
     if (!_poll()) {
-      cerr << "_poll() failed.\n";
+      ar_log_warning() << "arSerialSwitchDriver _poll() failed.\n";
       _stopped = true;
     }
-    if (_stopped) {
-      _eventThreadRunning = false;
-      stop();
-      return;
-    }
   }
+  _eventThreadRunning = false;
+  stop();
 }
 
 bool arSerialSwitchDriver::stop() {
@@ -131,28 +124,29 @@ bool arSerialSwitchDriver::stop() {
 bool arSerialSwitchDriver::_poll( void ) {
   if (_stopped)
     return false;
+
   if (_comPort.ar_write( &_outChar, 1 ) != 1) {
-    cerr << "ar_write failed.\n";
+    ar_log_warning() << "arSerialSwitchDriver ar_write failed.\n";
     return false;
   }
   if (_comPort.ar_read( &_inChar, 1) != 1) {
-    cerr << "ar_read failed.\n";
+    ar_log_warning() << "arSerialSwitchDriver ar_read failed.\n";
     return false;
   }
+
   arSerialSwitchEventType switchState = (arSerialSwitchEventType)((_inChar == _outChar)+1);
   if (switchState != _lastState) {
     if (switchState & _eventType) {
       if (_lastEventTime.zero()) {
         _lastEventTime = ar_time();
       } else {
-        ar_timeval currTime = ar_time();
-        double diffTime = ar_difftime( currTime, _lastEventTime );
+        const ar_timeval now = ar_time();
+        double diffTime = ar_difftime( now, _lastEventTime );
         if (switchState == AR_OPEN_SWITCH_EVENT) {
           diffTime = -diffTime;
         }
-        sendAxis( 0, (float)(diffTime/1.e6) );
-//        cerr << diffTime << endl;
-        _lastEventTime = currTime;
+        sendAxis( 0, float(diffTime/1.e6) );
+        _lastEventTime = now;
       }
     }
     _lastState = switchState;
--- src/drivers/arSerialSwitchDriver.h
+++ src/drivers/arSerialSwitchDriver.h
@@ -4,25 +4,9 @@
 //********************************************************
 
 /*
-Cross-platform Flock-of-Birds driver. Jim Crowell, 10/02.
-Further modifications by Lee Hendrickson and Ben Schaeffer.
-Thanks to Bill Sherman's FreeVR library for providing important
-insight into the Flock's operation.
-
-A Flock can be set up in several ways:
-  - Standalone: a single unit hosts a bird and a transmitter via RS232.
-  - Flock, 1 RS232: several units, but only one is connected to
-    the host via RS232. The "master" unit connects to the host,
-    and has flock ID 1. Other units slave to the master via
-    the flock's own FBB interface. A transmitter will be connected to
-    the flock, but maybe not to the master unit. Also, some units may
-    have no birds, e.g. the unit with the transmitter.
-  - Flock, multiple RS232. The host communicates to multiple
-    units directly via multiple serial connections. Unimplemented.
-
-Within Syzygy, serial port numbers are 1-based:
-port 1 in Win32 is COM1, in Linux is /dev/ttys0.
-Not true?  InputDevices-Drivers.t2t and drivers/RS232Server.cpp disagree.
+Cross-platform serial-port-as-switch driver.
+Switch shorts pin 2 to pin 3, making the port a loopback device
+(whatever you write to it is what you read).
 */
 
 #ifndef AR_SerialSwitch_RS232_DRIVER_H
@@ -33,8 +17,6 @@ Not true?  InputDevices-Drivers.t2t and drivers/RS232Server.cpp disagree.
 
 #include "arDriversCalling.h"
 
-// Driver for Ascension's Flock of Birds magnetic motion tracker.
-
 void ar_SerialSwitchDriverEventTask(void*);
 
 SZG_CALL enum arSerialSwitchEventType {
@@ -43,7 +25,6 @@ SZG_CALL enum arSerialSwitchEventType {
     AR_CLOSED_SWITCH_EVENT = 2,
     AR_BOTH_SWITCH_EVENT = 3
   };
-
 
 class arSerialSwitchDriver: public arInputSource {
   friend void ar_SerialSwitchDriverEventTask(void*);
