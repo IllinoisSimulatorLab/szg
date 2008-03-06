a8fbd94d1daf280823ddc8a8b5d69c5d3e067592
camilleg <>
2008-03-06 22:39:55 +0000

    comment

--- src/utilities/dkillall.cpp
+++ src/utilities/dkillall.cpp
@@ -22,7 +22,6 @@ int main(int argc, char** argv){
   if (argc == 2) {
     launcher.setSZGClient(&szgClient);
     if (!launcher.setVircomp(argv[1])) {
-      // arAppLauncher already printed msg.
       return 1;
     }
   }
