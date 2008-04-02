0d1a00fc3e6b4926b20a7446903f06b9891ae5c9
camilleg <>
2008-04-02 01:44:53 +0000

    simplify

--- src/demo/parade/parade.cpp
+++ src/demo/parade/parade.cpp
@@ -32,77 +32,62 @@ bool inputEventQueueCallback( arSZGAppFramework& fw, arInputEventQueue& eventQue
   return true;
 }
 
-const int numCommando = 8;
-Commando* commando[numCommando];
-const static arMatrix4 localMatrix[2] = {
-  ar_translationMatrix(4, 5, -4),
-  ar_translationMatrix(4, 5, -6) };
-int localTransformID[2];
+int main(int argc, char** argv){
+  arDistSceneGraphFramework fw;
+  if (!fw.init(argc, argv))
+    return 1;
 
-void worldInit(arDistSceneGraphFramework& framework){
-  // Directional lights.
+  // Init world.
+
   dgLight("light0", "root", 0, arVector4(0,0, 1,0), arVector3(1,1,1)); // point down, -z
   dgLight("light1", "root", 1, arVector4(0,0,-1,0), arVector3(1,1,1)); // point down, +z
 
   // Only for graphics, not for sound.
-  const string navNodeName = framework.getNavNodeName();
-  localTransformID[0] = dgTransform("local0", navNodeName, localMatrix[0]);
-  localTransformID[1] = dgTransform("local1", navNodeName, localMatrix[1]);
+  const string navNodeName = fw.getNavNodeName();
+  (void)dgTransform("local0", navNodeName, ar_translationMatrix(4, 5, -4));
+  (void)dgTransform("local1", navNodeName, ar_translationMatrix(4, 5, -6));
 
+  const int numCommando = 8;
   ARfloat color[numCommando][3]= {
-    {0.5,  0.5,  0.9 },
-    {0.5,  0.9,  0.5 },
-    {0.9,  0.5,  0.5 },
-    {0.5,  0,    0.5 },
-    {0.75, 0.75, 0   },
-    {0.75, 0,    0.75},
-    {0,    0.75, 0.75},
-    {0.75, 1,    0.75}
+    {0.5, 0.5, 0.9 },
+    {0.5, 0.9, 0.5 },
+    {0.9, 0.5, 0.5 },
+    {0.5, 0,   0.5 },
+    {0.7, 0.7, 0   },
+    {0.7, 0,   0.75},
+    {0,   0.7, 0.75},
+    {0.7, 1,   0.75}
   };
   const float paces[numCommando] = { .99, .04, .08, .12, .99, .04, .08, .12 };
 
-  for (int i=0; i<numCommando; ++i) {
+  Commando* commando[numCommando];
+  int i;
+  for (i=0; i<numCommando; ++i) {
     commando[i] = new Commando('y', color[i]);
     commando[i]->attachMesh("commando " + ar_intToString(i),
       i<4 ? "local0" : "local1");
     commando[i]->pace(paces[i], 80, 0.10);
   }
-}
 
-void worldAlter(void* f)
-{
-  arDistSceneGraphFramework* framework = (arDistSceneGraphFramework*) f;
-  // Invoke inputEventQueueCallback.
-  framework->processEventQueue();
-
-  for (int i=0; i<numCommando; ++i)
-    commando[i]->execute();
-  dgTransform(localTransformID[0], localMatrix[0]);
-  dgTransform(localTransformID[1], localMatrix[1]);
-}
-
-int main(int argc, char** argv){
-  arDistSceneGraphFramework framework;
-  if (!framework.init(argc, argv))
-    return 1;
-
-  framework.setNavTransSpeed(4.);
-  worldInit(framework);
-  framework.setEventQueueCallback( inputEventQueueCallback );
-  framework.ownNavParam( "translation_speed" );
-  if (!framework.start())
+  fw.setNavTransSpeed(4.);
+  fw.setEventQueueCallback( inputEventQueueCallback );
+  fw.ownNavParam( "translation_speed" );
+  if (!fw.start())
     return 1;
 
   // Disembodied hum coming from the floor, as an approximation to everywhere.
   // Use .wav not .mp3, to avoid a click when the loop wraps around.
   const arVector3 xyz(0,-5,0);
-  (void)dsLoop("unchanging", framework.getNavNodeName(), "parade.wav", 1, 0.6, xyz);
+  (void)dsLoop("unchanging", fw.getNavNodeName(), "parade.wav", 1, 0.6, xyz);
 
   while (true) {
-    ar_usleep(1000000/30); // 30 fps cpu throttle
-    worldAlter(&framework);
-    framework.setViewer();
-    framework.setPlayer();
+    ar_usleep(1000000/45);  // 45 fps cpu throttle
+    fw.processEventQueue(); // Invoke inputEventQueueCallback.
+    // Alter world.
+    for (i=0; i<numCommando; ++i)
+      commando[i]->execute();
+    fw.setViewer();
+    fw.setPlayer();
   }
   return 0;
 }
