998e7d1cef90c0106c5a0d9bd6be60a4e48d4149
camilleg <>
2008-04-30 23:18:26 +0000

    optimize and clarify tag-parsing

--- src/language/arXMLUtilities.cpp
+++ src/language/arXMLUtilities.cpp
@@ -44,28 +44,27 @@ bool ar_ignoreWhitespace(arTextStream* textStream,
   return true;
 }
 
-// Record characters from an arTextStream between the current
-// position and the next '<'. Stuff these characters
-// into the resizable buffer passed-in by the caller, null-terminated.
-// The next character returned
-// from the text stream will be '<' because of an ar_ungetc().
+// Record characters from an arTextStream up to next '<'.
+// Stuff these characters into the resizable buffer passed-in by the caller,
+// null-terminated.  The next character returned from the text stream will be
+// '<' because of an ar_ungetc().
+//
 // Return false iff EOF occurs before the next '<'.
 // The parsers for various field types call this first, and then
-// parse the string into their types, e.g. a sequence of floats).
+// parse the string into their types, e.g. a sequence of floats.
 //
-// By default, start at the beginning of the buffer.
-// But sometimes it's better to concatenate: if so, set "concatenate" to true.
+// Print no diagnostics - leave that up to the caller.
 bool ar_getTextBeforeTag(arTextStream* textStream, arBuffer<char>* textBuffer,
                          bool concatenate){
   int ch = ' ';
   if (!concatenate){
+    // Start at the beginning of the buffer.
     textBuffer->pushPosition = 0;
   }
   while (ch != '<'){
     ch = textStream->ar_getc();
     if (ch == EOF){
-      // DO NOT PRINT ANYTHING HERE! LET THE CALLER PRINT AN ERROR 
-      // MESSAGE!
+      // Found no tag.
       return false;
     }
     if (ch != '<'){
@@ -89,46 +88,43 @@ bool ar_getTextBeforeTag(arTextStream* textStream, arBuffer<char>* textBuffer,
 bool ar_getTextUntilEndTag(arTextStream* textStream,
                            const string& endTag,
 			   arBuffer<char>* textBuffer){
-  // We always start at the beginning of the buffer in this case.
+  // Start at the beginning of the buffer.
   textBuffer->pushPosition = 0;
-  string tagText;
-  int finalPosition = 0;
-  while (true){
+  while (true) {
     // Get text until the next tag.
     if (!ar_getTextBeforeTag(textStream, textBuffer, true)){
       return false;
     }
-    // Get the next tag. Note how we save the position.
-    finalPosition = textBuffer->pushPosition;
-    tagText = ar_getTagText(textStream, textBuffer, true);
+    // Get the next tag. Save the position.
+    const int finalPosition = textBuffer->pushPosition;
+    const string tagText(ar_getTagText(textStream, textBuffer, true));
     if (tagText == "NULL"){
       // There has been an error!
       return false;
     }
-    // Is it our end tag? If so, break.
     if (tagText == "/"+endTag){
-      break;
+      // End tag.
+      // NULL-terminate the array; use the position before the final tag.
+      // Since the final tag has at least 4 characters, the array need not grow.
+      textBuffer->data[finalPosition] = '\0';
+      return true;
     }
   }
-  // NULL-terminate the array; We use the position before
-  // the final tag. Since the final tag has at least 4 characters, we
-  // do not need to grow the array.
-  textBuffer->data[finalPosition] = '\0';
-  return true;
 }
 
-// Gets the text of the next tag, leaving the arTextStream at the character
-// after the closing '>'. For instance "<foo>" returns "foo" and 
-// "</foo>" returns "/foo". The caller passes in a resizable work buffer
-// to use in parsing the variable length string.
-// NOTE: returns "NULL" on error. And ignores initial whitespace.
-// NOTE: THIS IS NOT QUITE CORRECT!!! SPECIFICALLY, WE SHOULD IGNORE
-//   INITIAL AND TRAILING WHITESPACE AND ONLY SUPPORT TAG NAMES WITH
-//   NO WHITESPACE!
+// Get the text of the next tag, leaving the arTextStream just past the closing '>'.
+// For instance "<foo>" returns "foo" and "</foo>" returns "/foo".
+// The caller supplies a resizable buffer for parsing the variable-length string.
+//
+// Returns "NULL" on error.
+// Ignores initial whitespace.
+//
+// Bug: should ignore trailing whitespace.
+// Bug: should reject tag names which include whitespace.
 // 
 // Sometimes we want to be able to record the entire text stream in
 // our buffer. The default is NOT to do so, because the concatenate 
-// parameter is false unless explicitly set.
+// parameter defaults to false.
 //
 // Print no diagnostics - leave that up to the caller.
 string ar_getTagText(arTextStream* textStream, arBuffer<char>* buffer,
@@ -145,32 +141,37 @@ string ar_getTagText(arTextStream* textStream, arBuffer<char>* buffer,
   }
   int ch = textStream->ar_getc();
   if (ch == EOF){
+    // Reasonable end of file.
     return string("NULL");
   }
   if (ch != '<'){
+    // Missing start of tag.
     return string("NULL");
   }
-  else if (concatenate){
-    // Record the '<'.
+
+  if (concatenate){
+    // Record the start-of-tag '<'.
     buffer->push(char(ch));
-  }
-  if (!concatenate){
+  } else {
     // Begin packing at the beginning.
     buffer->pushPosition = 0;
   }
   const int tagStart = buffer->pushPosition;
-  while (ch != '>'){
+  while (true) {
     ch = textStream->ar_getc();
     if (ch == EOF){
+      // EOF during tag
       return string("NULL");
     }
-    if (ch != '>'){
-      buffer->push(char(ch));
+    if (ch == '>'){
+      break;
     }
+    buffer->push(char(ch));
   }
+  // Reached end of tag.
   const int tagLength = buffer->pushPosition - tagStart;
   if (concatenate){
-    // Record the final '>'.
+    // Record the end-of-tag '>'.
     buffer->push(char(ch));
   }
   // Caller might treat result as a C string, so null-terminate it.
@@ -185,18 +186,15 @@ string ar_getTagText(arTextStream* textStream){
   return ar_getTagText(textStream, &buffer);
 }
 
-// Should ususally operate on the return value from ar_getTagText(...).
-// Returns true if the first character is '/'.
+// Reports if the first character is '/'.
+// Ususally called on ar_getTagText()'s return value.
 bool ar_isEndTag(const string& tagText){
-  if (tagText.length() >= 1 && tagText[0] == '/'){
-    return true;
-  }
-  return false;
+  return !tagText.empty() && tagText[0] == '/';
 }
 
-// Returns the tag type, irrespective of whether it is a beginning or
+// Return the tag type, irrespective of whether it is a beginning or
 // an ending tag. So "foo" and "/foo" both return "foo".
 string ar_getTagType(const string& tagText){
-  return (tagText.length() >= 1 && tagText[0] == '/') ?
+  return ar_isEndTag(tagText) ?
     tagText.substr(1,tagText.length()-1) : tagText;
 }
