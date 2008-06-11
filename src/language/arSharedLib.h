1c525ab7a851d6aae896bd1f7ce51f68b3c561af
camilleg <>
2008-06-11 03:01:23 +0000

    comments

--- src/language/arSharedLib.h
+++ src/language/arSharedLib.h
@@ -7,7 +7,6 @@
 #define AR_SHARED_LIB_H
 
 #ifdef AR_USE_WIN_32
-// DO NOT INCLUDE <windows.h> here! INSTEAD DO AS BELOW...
 #ifdef AR_USE_MINGW
 #include <windows.h>
 #else
@@ -30,22 +29,20 @@ typedef void* LibHandle;
 
 #ifdef AR_USE_DARWIN
 typedef void* LibHandle;
-// dlcompat wrapper over Mac OS X's native dylib API
+// dlcompat wrapper around native dylib API
 #include <dlfcn.h>
 #endif
 
 #include <string>
 using namespace std;
 
-// A wrapper class that allows portable use of shared libraries.
-// It handles the different naming conventions and behaviors for
-// different platforms as automatically as is possible. It also 
-// offers syzygy-aware functions for dll-loading, as is needed, for
-// instance, in managing load paths across different architectures.
-// A case in point: linux and irix require, essentially, that dll's
-// loaded without absolute path names either be on the LD_LIBRARY_PATH
-// or in a standard directory, whereas Win32 will look first for dll's
-// in the executable's directory.
+// Wrapper class for portable use of shared libraries.
+// Hide platforms' different naming conventions and behaviors.
+// Offer syzygy-aware functions for dll-loading, as is needed, for
+// instance, in managing load paths.  Example: linux and irix require
+// that dll's lacking absolute path names be either on
+// LD_LIBRARY_PATH or in a standard directory,
+// but win32 first checks the exe's directory.
 
 typedef void* (*arSharedLibFactory)();
 typedef void (*arSharedLibObjectType)(char*, int);
